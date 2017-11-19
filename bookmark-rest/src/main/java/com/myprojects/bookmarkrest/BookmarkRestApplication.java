7a5736f435d7fdbc392aa6077b81407b7d443b2e
AntonManin <antoxa3030@yandex.ru>
2017-11-19 17:03:34 +0100

    Initialized Spring Boot projec

---
+++ bookmark-rest/src/main/java/com/myprojects/bookmarkrest/BookmarkRestApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.bookmarkrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkRestApplication.class, args);
+	}
+}
